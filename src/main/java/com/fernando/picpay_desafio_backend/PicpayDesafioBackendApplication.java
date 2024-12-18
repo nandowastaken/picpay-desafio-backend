5f7cdebbbc5b755a2119315f671066e6dcc88b63
nandowastaken <fernandojorge.cavalcantegomes@gmail.com>
2024-12-18 11:21:58 -0300

    feat: spring files

---
+++ src/main/java/com/fernando/picpay_desafio_backend/PicpayDesafioBackendApplication.java
@@ -0,0 +1,13 @@
+package com.fernando.picpay_desafio_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicpayDesafioBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PicpayDesafioBackendApplication.class, args);
+	}
+
+}
